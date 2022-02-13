ef8d82fee0d04b2387232d20c282649fb983a1a2
kappapiana <carlo@piana.eu>
2022-02-13 10:59:13 +0100

    Revert "Add features to anonymize.py"

--- anonymize.py
+++ anonymize.py
@@ -32,7 +32,9 @@ import re
 import shutil
 import mimetypes
 from pathlib import Path
-import argparse
+
+cwd = Path.cwd()  # Current directory is cwd
+
 
 # this is just optional in case we want colors
 class bcolors:
@@ -47,98 +49,21 @@ class bcolors:
     UNDERLINE = '\033[4m'
 
 
-class File():
-    '''Stores information relevant to each file that is being anonymized.
+# Get the first argument as the modified file
+zipped_file_name = sys.argv[1]
 
-    Class has following properties:
-    name: filename
-    file_type: docx or odt
-    tmp_dir: location of unzipped file
-    '''
-
-    def __init__(self, name, tmp_dir):
-        '''Initializes name, tmp_dir, and file_type and
-        unzips original file into temporary directory'''
-        self.name = name
-        self.tmp_dir = cleanup_dir(tmp_dir)
-
-        self.file_type = unzip_file(name, tmp_dir)
-
-        # we establish what kind of string is the original file
-        if self.file_type == "odt":
-            self.set_odt_strings()
-        elif self.file_type == "docx":
-            self.set_docx_strings()
-        else:
-            print("It's a monkey!")
-            sys.exit('not do')
-            cleanup_dir(args.tmp_dir)
+# Set the relevant part to be changed in file source
+odt_string = "<dc:creator>(.*?)</dc:creator>"
+odt_string_initials = "<meta:creator-initials>(.*?)</meta:creator-initials>"
+odt_string_initials_replaced = "<meta:creator-initials></meta:creator-initials>"
+doc_string = "w:author=\"(.*?)\""
+doc_string_initials = "w:initials=\"(.*?)\""
+doc_string_initials_replaced = "w:initials=\"\""
 
 
-    def set_odt_strings(self):
-        self.author_string = "<dc:creator>(.*?)</dc:creator>"
-        self.initials = "<meta:creator-initials>(.*?)</meta:creator-initials>"
-        self.initials_replaced = "<meta:creator-initials></meta:creator-initials>"
-        self.dates = "<dc:date>(.*?)</dc:date>"
-        self.dates_replaced = "<dc:date></dc:date>"
-        self.textfiles = [os.path.join(self.tmp_dir, "content.xml")]
-        self.comments_index = 0 # For initials deletion
-
-    def set_docx_strings(self):
-        self.author_string = "w:author=\"(.*?)\""
-        self.initials = "w:initials=\"(.*?)\""
-        self.initials_replaced = "w:initials=\"\""
-        self.dates = "w:date=\"(.*?)\""
-        self.dates_replaced = "w:date=\"\""
-        self.textfiles = [os.path.join(self.tmp_dir, 'word', xml)
-                          for xml in ["comments.xml", "document.xml"]]
-        self.comments_index = 0 # For initials deletion
-
-    def replace(self, from_string, to_string):
-        for textfile in self.textfiles:
-            with open(textfile, 'r') as f:
-                file_contents = replace_text(f.read(), from_string, to_string)
-            with open(textfile, 'w') as f:
-                f.write(file_contents)
-
-    def find_authors(self):
-        for textfile in self.textfiles:
-            with open(textfile, 'r') as f:
-                return set(re.findall(self.author_string, f.read()))
-
-    def delete_initials(self):
-        '''replaces the content of the initials tag with an empty string. It doesn't
-        ask for permission though, returns nothing'''
-        with open(self.textfiles[self.comments_index], "r") as f:
-            replaced = replace_text(f.read(), self.initials, self.initials_replaced)
-        with open(self.textfiles[self.comments_index], "w") as f:
-            f.write(replaced)
-
-    def delete_dates(self):
-        '''replaces the content of the date tag with an empty string. It doesn't
-        ask for permission though, returns nothing'''
-        with open(self.textfiles[self.comments_index], "r") as f:
-            replaced = replace_text(f.read(), self.dates, self.dates_replaced)
-        with open(self.textfiles[self.comments_index], "w") as f:
-            f.write(replaced)
-
-    def rezip(self, output_prefix, output_dir):
-        # Recreate a version of the file with the new content in it
-
-        output_file = os.path.join(output_dir, output_prefix + self.name)
-        shutil.make_archive(output_file, "zip", self.tmp_dir)
-
-        output_file_zip = output_file + ".zip"
-
-        # Rename it to the original extension
-        os.rename(output_file_zip, output_file)
-
-        # function returns the name of the changed file
-        return output_file
-
-
-def cleanup_dir(dir="/tmp/anonymize/"):
-    ''' cleans the working directory '''
+def cleanup_dir(dir="/tmp/test/"):
+    ''' cleans the working directory, using the global variable
+    no need to pass arguments '''
 
     if os.path.isdir(dir) is True:
 
@@ -173,20 +98,21 @@ def cleanup_dir(dir="/tmp/anonymize/"):
     return dir
 
 
-def unzip_file(orig_file, tmp_dir):
+def unzip_file(orig_file):
     '''odt and docx are zip. We send them to a convenient location and work on
     that extracted stuff. Also, we check if the file type is correct, else, we
     abort, since we don't know how to handle different files'''
 
     type = mimetypes.guess_type(orig_file)
     if type[0] == "application/vnd.oasis.opendocument.text":
+        print("It's a boy!")
         filetype = "odt"
     elif type[0] == "application/vnd.openxmlformats-officedocument.wordprocessingml.document":
         filetype = "docx"
     else:
         print("It's a monkey!")
 
-    shutil.unpack_archive(orig_file, tmp_dir, 'zip')
+    shutil.unpack_archive(zipped_file_name, export_dir, 'zip')
     return filetype
 
 
@@ -198,18 +124,42 @@ def replace_text(target_string, from_string, to_string):
 
     return search_replace
 
-def cycle_ask(cur_files):
+def create_megastring(unzipped_files):
+
+    '''accepts an array of files and appends the content, so we search
+    everything'''
+
+    target_string = "" # create the variable as an empty string
+    counter = 0
+
+
+    for file in unzipped_files:
+
+        if os.path.exists(file):
+
+            with open(file, 'r') as f:
+                target_string_temp = f.read()
+                target_string = f'{target_string} {target_string_temp}'
+                changed_text = ""  # this will avoid errors when quitting without changing
+                counter +=1
+
+        else :
+            print("\nThe search file is missing:", file)
+
+    return(target_string)
+
+
+def cycle_ask(cur_filename):
     '''opens and reads file to be changed, asks for input until user is fine
     then writes the changed string, until you call it quits. This is sort of the
     main function here'''
 
-    # Flatten list of lists structure
-    filelist = [item for subfile in cur_files for item in subfile.textfiles]
 
     while True:
 
-        # here we find the authors
-        authors_list = find_all_authors(cur_files)
+         # here we find
+
+        authors_list = find_authors(create_megastring(cur_filename))
 
         # Add more commands to the list of possible authors
         additional_commands = {
@@ -237,9 +187,20 @@ def cycle_ask(cur_files):
         # If you select quit, we are over
         elif from_string == "quit":
             # We have finished changing the target string, write it into file.
-            print("\n++++++++++++++++++++++++++++++++++++")
-            print(f"\n{bcolors.BOLD}*** we have not changed anything{bcolors.ENDC} *** \n")
-            print("++++++++++++++++++++++++++++++++++++\n")
+            print("ok, we stop here")
+            try:
+                # if we have not done anything, this test fails, meaning we have
+                # not changed anything
+                test = target_string_local
+                len(test)
+
+            except Exception as e:
+                str(e)
+                # print(e) #for debugging the error, but it's intentional
+                print("\n++++++++++++++++++++++++++++++++++++")
+                print(f"\n{bcolors.BOLD}*** we have not changed anything{bcolors.ENDC} *** \n")
+                print("++++++++++++++++++++++++++++++++++++\n")
+
             break
 
         elif from_string == "all":
@@ -247,11 +208,20 @@ def cycle_ask(cur_files):
                   "\nPlease enter the string you want to change"
                   f" {bcolors.BOLD}to{bcolors.ENDC} \n")
 
-            from_string = "|".join(authors_list.values())
             to_string = input(":> ")
 
-            for cur_file in cur_files:
-                cur_file.replace(from_string, to_string)
+            for file in cur_filename:
+
+                with open(file, 'r') as k:
+                    target_string_local = k.read()
+
+                    for key in authors_list:
+                        from_string = authors_list.get(key)
+                        changed_text = replace_text(target_string_local, from_string, to_string)
+                        target_string_local = changed_text
+
+                with open(file, 'w') as k:
+                    k.write(changed_text)
 
         # othewise, you have selected a good key, let's replace it with
         # something and start over
@@ -259,92 +229,113 @@ def cycle_ask(cur_files):
             print(f"You have selected {from_string}")
             to_string = input(f"\nPlease enter the string you want to change {bcolors.BOLD}to{bcolors.ENDC} \n\n:> ")
 
-            for cur_file in cur_files:
-                cur_file.replace(from_string, to_string)
+            for file in cur_filename:
+
+                with open(file, 'r') as k:
+                    target_string_local = k.read()
+
+                    changed_text = replace_text(target_string_local, from_string, to_string)
+                    target_string_local = changed_text
+
+                with open(file, 'w') as k:
+                    k.write(changed_text)
 
 
-def rezip(cur_files, output_prefix, output_dir):
-    '''rewraps everything'''
-    return [f.rezip(output_prefix, output_dir) for f in cur_files]
+def rezip():
+    '''rewraps everyting'''
+
+    anon_textfile = os.path.join(cwd, '_anon_') + zipped_file_name
+
+    # Recreate a version of the file with the new content in it
+    shutil.make_archive(anon_textfile, "zip", export_dir)
+
+    anon_textfile_zip = anon_textfile + ".zip"
+
+    # Rename it to the original extension
+    os.rename(anon_textfile_zip, anon_textfile)
+
+    # function returns the name of the changed file
+    return anon_textfile
 
 
-def find_all_authors(cur_files):
+def find_authors(in_text):
     '''finds and lists authors in content xml file returning a dictionary of
     values: this is necessary to know what authors we have since last change '''
 
-    authors = [f.find_authors() for f in cur_files]
-    authors_list = sorted(set().union(*authors),
-                          key = lambda s: s.lower())  # sort in lowercase
-
-    # assign number to each option, starting from 1
-    authors_dict = {str(i+1): author
-                    for i, author in enumerate(authors_list)}
+    authors = set(re.findall(author_string, in_text))
+    author_list = sorted(authors, key= lambda s: s.lower())  # sort in lowercase
+    authors_dict = {}
+    # assign number to each option, increase for next iteration
+    counter = 1
+    for i in author_list:
+        index = str(counter)
+        authors_dict[index] = i
+        counter += 1
 
     return authors_dict
 
-def delete_initials(cur_files):
+def delete_initials(is_type):
     '''replaces the content of the initials tag with an empty string. It doesn't
     ask for permission though, returns nothing'''
 
-    for cur_file in cur_files:
-        cur_file.delete_initials()
 
-    print("\n++++++++++++++++++++++++++++++++++++")
-    print(f"\n{bcolors.OKCYAN}    we have deleted the initials{bcolors.ENDC}\n")
-    print("++++++++++++++++++++++++++++++++++++\n")
+    if is_type == "docx":
+        comments_file = os.path.join(export_dir, 'word', '') + "comments.xml"
+        initials_replaced = doc_string_initials_replaced
+        initials = doc_string_initials
+    elif is_type == "odt":
+        comments_file = export_dir + "content.xml"
+        initials_replaced = odt_string_initials_replaced
+        initials = odt_string_initials
 
-def delete_dates(cur_files):
-    '''Deletes the dates associated with comments, returns nothing'''
+    if os.path.exists(comments_file):
 
-    for cur_file in cur_files:
-        cur_file.delete_dates()
+        with open(comments_file, 'r') as file:
+            data = file.read()
+            data = replace_text(data, initials, initials_replaced)
 
-    print("\n++++++++++++++++++++++++++++++++++++")
-    print(f"\n{bcolors.OKCYAN}    we have deleted the dates{bcolors.ENDC}\n")
-    print("++++++++++++++++++++++++++++++++++++\n")
+        with open(comments_file, 'w') as file:
+            file.write(data)
 
-
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description="Anonymize documents.",
-                                     formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    parser.add_argument("filenames", metavar="FILE", type=str, nargs="+",
-                        help="Path to the files to anonymize")
-    parser.add_argument("--tmp_dir", type=str, default="/tmp/anonymize",
-                        help="Temporary directory to work with")
-    parser.add_argument("--output_prefix", type=str, default = "_anon_",
-                        help="Prefix for output files.")
-    parser.add_argument("--output_dir", type=str, default = Path.cwd(),
-                        help="Directory for output files")
-    parser.add_argument("--dates", action='store_true',
-                        help="Delete the dates as well")
-    args = parser.parse_args()
-
-    # if args.output is None:
-    #     cwd = Path.cwd()  # Current directory is cwd
-    #     args.output = os.path.join(cwd, "_anon_" + os.path.basename(args.filenames))
-
-    # Cleanup the main tmp_dir
-    cleanup_dir(args.tmp_dir)
-
-    # Make a tmp directory for each file and unzip the file there
-    # Creates a list of File
-    files = [File(filename, os.path.join(args.tmp_dir, str(i)))
-             for i, filename in enumerate(args.filenames)]
-
-    cycle_ask(files)
-
-    delete_initials(files)
-
-    if args.dates:
-        delete_dates(files)
-
-    anonymized_files = rezip(files, args.output_prefix, args.output_dir)
-    for anonymized in anonymized_files:
-        print(f"{bcolors.OKGREEN}file is now in {anonymized}{bcolors.ENDC}\n")
+            print("\n++++++++++++++++++++++++++++++++++++")
+            print(f"\n{bcolors.OKCYAN}    we have deleted the initials{bcolors.ENDC}\n")
+            print("++++++++++++++++++++++++++++++++++++\n")
 
 
+# Let's run it
+
+export_dir = cleanup_dir()
+
+file_type = unzip_file(zipped_file_name)
+
+# we establish what kind of string is the original file
+
+if file_type == "odt":
+    author_string = odt_string
+    textfile0 = export_dir + "content.xml"
+    textfile = [textfile0] #just to use a multifile structure, don't change
+elif file_type == "docx":
+    author_string = doc_string
+    textfile0 = os.path.join(export_dir, 'word', '') + "document.xml"
+    textfile1 = os.path.join(export_dir, 'word', '') + "comments.xml"
+    textfile = [textfile0, textfile1]
+
+else:
+    print("It's a monkey!")
+    sys.exit('not do')
     cleanup_dir()
 
-    # TODO:
+cycle_ask(textfile)
 
-    # Add create dir
+delete_initials(file_type)
+
+anonymized = rezip()
+
+print(f"{bcolors.OKGREEN}file is now in {anonymized}{bcolors.ENDC}\n")
+
+
+cleanup_dir()
+
+# TODO:
+
+# Add create dir
