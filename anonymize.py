b7cfb038562abbc29201ef68e7a906ea132f72b9
kappapiana <carlo@piana.eu>
2022-05-30 11:39:31 +0200

    fix: Add wrong filetype to error message
    Note that unzip_file did not return the file type: added explicit return

--- anonymize.py
+++ anonymize.py
@@ -72,7 +72,7 @@ class File():
         elif self.file_type == "docx":
             self.set_docx_strings()
         else:
-            sys.exit(f"{bcolors.FAIL}{bcolors.BOLD}Error:{bcolors.ENDC} {name} is a non supported file type")
+            sys.exit(f"{bcolors.FAIL}{bcolors.BOLD}Error:{bcolors.ENDC} {name} is not a supported file type: we found \"{self.file_type}\"")
             cleanup_dir(args.tmp_dir)
 
         self.check_textfiles()
@@ -221,7 +221,7 @@ def unzip_file(orig_file, tmp_dir):
     elif type[0] == "application/vnd.openxmlformats-officedocument.wordprocessingml.document":
         filetype = "docx"
     else:
-        return
+        return type[0]
 
     shutil.unpack_archive(orig_file, tmp_dir, 'zip')
     return filetype
