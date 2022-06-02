13258c6285c43ca9ca9ac7ab4023e980e76b6132
kappapiana <carlo@piana.eu>
2022-06-02 13:57:47 +0200

    feature: Keep dates as default (#25)
    * feature: Keep dates as default
    
    Co-authored-by: Yaman Qalieh <ybq987@gmail.com>

--- anonymize.py
+++ anonymize.py
@@ -369,10 +369,10 @@ if __name__ == '__main__':
                         help="Prefix for output files.")
     parser.add_argument("--output-dir", type=str, default = Path.cwd(),
                         help="Directory for output files")
-    parser.add_argument("--keep-dates", action="store_true",
-                        help="Keep the comment dates")
+    parser.add_argument("--remove-dates", action="store_true",
+                        help="Remove dates from tracked edits")
     parser.add_argument("--keep-initials", action="store_true",
-                        help="Keep the commenter initials")
+                        help="Keep the comment authors' initials")
     args = parser.parse_args()
 
     # Cleanup the main tmp_dir
@@ -388,7 +388,7 @@ if __name__ == '__main__':
     if not args.keep_initials:
         delete_initials(files)
 
-    if not args.keep_dates:
+    if args.remove_dates:
         delete_dates(files)
 
     anonymized_files = rezip(files, args.output_prefix, args.output_dir)
